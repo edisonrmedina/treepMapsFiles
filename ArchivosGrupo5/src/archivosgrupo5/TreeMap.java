585b8505ac5181341a6300a462a62934fb4fa345
NahinMontoya <73256114+NahinMontoya@users.noreply.github.com>
2021-01-21 13:33:32 -0500

    Modificando las clases TreeMap y TreeNode

--- ArchivosGrupo5/src/archivosgrupo5/TreeMap.java
+++ ArchivosGrupo5/src/archivosgrupo5/TreeMap.java
@@ -1,61 +1,50 @@
 
 package archivosgrupo5;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Stack;
 
 
-/**
- *
- * @author fioye
- */
-public class TreeMap<T> {
-    private TreeNode<T> root;
 
-    public TreeMap(T content,float size,String name) {
-        this.root=new TreeNode(content,size,name);
+public class TreeMap {
+    
+    private TreeNode raiz;
+    
+    public TreeMap()
+    {
+        this.raiz = new TreeNode();
     }
     
-    public TreeNode<T> getRoot() {
-        return root;
-    }
-
-    public void setRoot(TreeNode<T> root) {
-        this.root = root;
-    }
-    public boolean isEmpty(){
-        return this.root==null;
-    }
-    private boolean isLeaf(){
-        return this.root.getChildren().isEmpty();
+    public TreeMap(String nombre)
+    {
+        this.raiz = new TreeNode(nombre);
     }
     
-    @Override
-    public String toString() {
-        return "TreeMap{" + "root=" + root + '}';
-    }    
-
-    public List<TreeMap<T>> recorrerEnAnchura(TreeMap<T> root){
-        List<TreeMap<T>> recorrido=new LinkedList<>();
-        Queue<TreeMap<T>> cola=new LinkedList<>();
-        cola.add(root);
-        root.root.setVisited(true);
-        while(!cola.isEmpty()){
-            TreeMap<T> nodo=cola.remove();
-            recorrido.add(nodo);
-            List<TreeMap<T>> hijos=nodo.getRoot().getChildren();
-            for(TreeMap<T> hijo: hijos){
-                if(!hijo.getRoot().isVisited()){
-                    cola.add(hijo);
-                    hijo.getRoot().setVisited(true);
-                    if(hijo.isLeaf())
-                        hijo.getRoot().setIsDirectory(false);
-                }
-            }
-        }
-        return recorrido;
+    public TreeMap(String ruta, double tamaño, String nombre)
+    {
+        this.raiz = new TreeNode(ruta, tamaño, nombre);
     }
     
+    public void añadirHijo(TreeMap hijo)
+    {
+        this.raiz.añadirHijo(hijo);
+    }
+
+    public TreeNode getRaiz() 
+    {
+        return raiz;
+    }
+
+    public void setRaiz(TreeNode raiz) 
+    {
+        this.raiz = raiz;
+    }
+    
+    public boolean tieneHijos()
+    {
+        return this.raiz.tieneHijos();
+    }
 }
--- ArchivosGrupo5/src/archivosgrupo5/TreeNode.java
+++ ArchivosGrupo5/src/archivosgrupo5/TreeNode.java
@@ -1,93 +1,96 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package archivosgrupo5;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author fioye
- */
-public class TreeNode<T> {
-    private List<TreeMap<T>> children = new ArrayList<>();
-    private T content;
-    private String name;
-    private float size = 0;
-    private boolean visited=false;
-    private boolean isDirectory;
 
-    public boolean isVisited() {
-        return visited;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setVisited(boolean visited) {
-        this.visited = visited;
-    }
-
-    public TreeNode(T content, float size, String name) {
-        this.content = content;
-        this.size = size;
-        this.name=name;
-    }
-
-    public void addChild(TreeMap child) {
-        children.add(child);
-    }
-
-    public T getContent() {
-        return this.content;
-    }
-
-    public float getSize() {
-        return size;
-    }
-
-    public boolean isIsDirectory() {
-        return isDirectory;
-    }
-
-    public void setIsDirectory(boolean isDirectory) {
-        this.isDirectory = isDirectory;
+public class TreeNode {
+    private String ruta;
+    private double tamañoFichero;
+    private String nombre;
+    private List<TreeMap> hijos;
+    private boolean visitado;
+    
+    public TreeNode()
+    {
+        this.hijos = new ArrayList<>();
     }
     
-    public void setSize(float size) {
-        this.size = size;
+    public TreeNode(String nombre)
+    {
+        this.ruta = nombre;
+        this.visitado = false;
+        this.hijos = new ArrayList<>();
+    }
+     
+    public TreeNode(String ruta, double tamañoFichero, String nombre)
+    {
+        this.ruta = ruta;
+        this.tamañoFichero = tamañoFichero;
+        this.nombre = nombre;
+        this.visitado = false;
+        this.hijos = new ArrayList<>();
     }
 
-    public void setChildren(List<TreeMap<T>> children) {
-        this.children = children;
+    public String getRuta() 
+    {
+        return ruta;
     }
 
-    public void setContent(T content) {
-        this.content = content;
-    }
- 
-    public List<TreeMap<T>> getChildren() {
-        return children;
+    public void setRuta(String ruta) 
+    {
+        this.ruta = ruta;
     }
 
-    @Override
-    public String toString() {
-        return "TreeNode{" + "children=" + children + ", content=" + content + ", size=" + size + '}';
+    public double getTamañoFichero() 
+    {
+        return tamañoFichero;
     }
 
-    private boolean isDirectory() {
-        return getChildren()==null;
+    public void setTamañoFichero(double tamañoFichero) 
+    {
+        this.tamañoFichero = tamañoFichero;
+    }
+
+    public List<TreeMap> getHijos() 
+    {
+        return hijos;
+    }
+
+    public void setHijos(List<TreeMap> hijos) 
+    {
+        this.hijos = hijos;
+    }
+
+    public boolean isVisitado() 
+    {
+        return visitado;
+    }
+
+    public void setVisitado(boolean visitado) 
+    {
+        this.visitado = visitado;
     }
     
+    public void añadirHijo(TreeMap hijo)
+    {
+        this.hijos.add(hijo);
+    }
     
+      public String getNombre() 
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) 
+    {
+        this.nombre = nombre;
+    }
     
+    public boolean tieneHijos()
+    {
+        return this.hijos.size()>0;
+    }
 }
